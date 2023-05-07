2c60169e0e199f763dba877ffd0fb57dcbb2f1ca
Simon Yawin <simyawin@gmail.com>
2023-05-07 15:14:21 -0400

    implement stack using circular linked list.

---
+++ src/linkedlist/CircleStack.java
@@ -0,0 +1,43 @@
+package linkedlist;
+
+public class CircleStack {
+
+    //stack is FIFO
+
+    private final CircleLinkedList linkedList;
+
+    public CircleStack() {
+        linkedList = new CircleLinkedList();
+    }
+
+    public void push(int iData, double dData) {
+        linkedList.insert(iData, dData);
+    }
+
+    public Link pop() {
+        return linkedList.delete();
+    }
+
+    private void print() {
+        linkedList.print();
+    }
+
+    private static class CircleStackDemo {
+        public static void main(String[] args) {
+            CircleStack stack = new CircleStack();
+            stack.push(22, 2.99);
+            stack.print();
+            stack.push(33, 3.81);
+            stack.print();
+            stack.push(44, 4.90);
+            stack.print();
+            stack.push(55, 1.01);
+            stack.print();
+            Link pop = stack.pop();
+            //fails, returns 22 which is key of first pushed link
+            assert pop.iData == 55;
+            System.out.println(pop);
+            stack.print();
+        }
+    }
+}
