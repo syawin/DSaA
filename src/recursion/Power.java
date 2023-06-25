2a6716b76c2d5257588ff6c5e0f95e62d1792830
Simon Yawin <simyawin@gmail.com>
2023-06-25 14:01:50 -0400

    Add explanatory notes to recursive Power

--- src/recursion/Power.java
+++ src/recursion/Power.java
@@ -1,6 +1,9 @@
 package recursion;
 
 public class Power {
+    //It's important to note that this implementation is more efficient than the naive approach
+    // (e.g. pow(x, y) = x * pow(x,y-1)) as each iteration cuts y in half.
+    // This results in O(log N) time complexity.
     public static int power(int x, int y) {
         System.out.println("x = " + x + ", y = " + y);
         if (y == 1) return x;
