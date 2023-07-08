266c51fafc01c81c80140a55f17fef6df4b57956
Simon Yawin <simyawin@gmail.com>
2023-07-08 18:50:10 -0400

    Reformat stack class

--- src/stack/StackI.java
+++ src/stack/StackI.java
@@ -1,44 +1,37 @@
 package stack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Reversing a String with Stacks
 // Stack push/pop = O(1)
 @SuppressWarnings("BooleanMethodIsAlwaysInverted")
 public class StackI {
+
     private final int maxSize;
     private int[] stackArray;
     private int top;
 
-    public StackI(int maxSize) {
-        this.maxSize = maxSize;
+    public StackI(int maxSize)
+    {
+        this.maxSize    = maxSize;
         this.stackArray = new int[this.maxSize];
-        this.top = -1;
+        this.top        = -1;
     }
 
-    public void push(int c) {
+    public StackI push(int c)
+    {
         stackArray[++top] = c;
+        return this;
     }
 
-    public int pop() {
-        return stackArray[top--];
-    }
-
-    public int peek() {
+    public int peek()
+    {
         return stackArray[top];
     }
 
-    public String peekN(int n) {
-        return String.valueOf(stackArray[n]);
-    }
-
-    public boolean isEmpty() {
-        return top == -1;
-    }
-
-    public int size() {
-        return top + 1;
-    }
-
-    public void displayStack(String s) {
+    public void displayStack(String s)
+    {
         System.out.print(s);
         System.out.print("Stack (bottom->top):\t");
         for (int i = 0; i < size(); i++) {
@@ -46,4 +39,34 @@ public class StackI {
         }
         System.out.println();
     }
+
+    public int size()
+    {
+        return top + 1;
+    }
+
+    public String peekN(int n)
+    {
+        return String.valueOf(stackArray[n]);
+    }
+
+    public List<Integer> popAll()
+    {
+        List<Integer> list = new ArrayList<>();
+        while (!isEmpty()) {
+            list.add(pop());
+        }
+        return list;
+    }
+
+    public boolean isEmpty()
+    {
+        return top == -1;
+    }
+
+    public int pop()
+    {
+        return (top >= 0) ? stackArray[top--] : 0;
+    }
+
 }
