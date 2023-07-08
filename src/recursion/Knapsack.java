80f5e51dfb7a737966deb97cdc57e4ee68d08bae
Simon Yawin <simyawin@gmail.com>
2023-07-08 18:51:12 -0400

    Create recursive knapsack program

---
+++ src/recursion/Knapsack.java
@@ -0,0 +1,60 @@
+package recursion;
+
+import stack.StackI;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Knapsack {
+
+    public static List<Integer> knapsack(List<Integer> list, int target)
+    {
+        StackI stackI = new StackI(list.size());
+        List<Integer> ans = Collections.emptyList();
+        if (knapRecur(list, target, stackI)) ans = stackI.popAll();
+        return Collections.unmodifiableList(ans);
+    }
+
+    public static boolean knapRecur(List<Integer> arr, int target, StackI ans)
+    {
+        if (arr == null || arr.isEmpty()) {
+            return false;
+        }
+        // while has next, get elem & compare to target
+        for (int i = 0; i < arr.size(); i++) {
+            int elem = arr.get(i);
+            if (elem == target) {
+                ans.push(elem);
+                return true;
+            }
+            else if (elem < target) {
+                /*
+                For some reason the "3,4,5" target 10 scenario isn't working. Getting an off-by-one type error
+                where sublist is duplicating elements already picked. Yet changing the code below to do a proper
+                sublist by increasing index causes the happy path to fail.
+                */
+                if (knapRecur(arr.subList(1, arr.size()), target - elem, ans.push(elem))) {
+                    return true;
+                }
+                else {
+                    ans.pop();
+                }
+            }
+        }
+        return false;
+    }
+
+    private static class KnapsackDemo {
+
+        public static void main(String[] args)
+        {
+            System.out.println(knapsack(Arrays.asList(8, 4, 2, 1), 5));
+            System.out.println(knapsack(Arrays.asList(1, 2, 8, 4), 5));
+            System.out.println(knapsack(Arrays.asList(12, 7, 11, 8, 9, 5), 20));
+            System.out.println(knapsack(Arrays.asList(3), 5));
+        }
+
+    }
+
+}
