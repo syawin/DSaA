574bdbd8d97b693693201db67210255ab998335c
Simon Yawin <simyawin@gmail.com>
2023-07-10 19:09:37 -0400

    Create stub combinations class

---
+++ src/recursion/TeamCombinations.java
@@ -0,0 +1,14 @@
+package recursion;
+
+import java.util.Collections;
+import java.util.List;
+
+public class TeamCombinations {
+
+    public static List<String> listAllCombinations(List<String> args, int size)
+    {
+        // todo implement recursive method to get all combinations of a target size from a given set of chars
+        return Collections.emptyList();
+    }
+
+}
