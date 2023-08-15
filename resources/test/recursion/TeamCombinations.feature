78a01fb5fc6e6092f37cb25fbb248dcfb1a81073
Simon Yawin <simyawin@gmail.com>
2023-08-15 18:35:40 -0400

    Finish combinations programming project. Remove testing code.

--- resources/test/recursion/TeamCombinations.feature
+++
@@ -1,8 +0,0 @@
-Feature: Team combinations
-    Enumerate all possible combinations into a group of size K from a set of N unique elements
-
-    Scenario:
-        Given a selection set "A,B,C,D,E"
-        And a team size 3
-        When I execute the team combo recursive algorithm
-        Then the result returned is "ABC,ABD,ABE,ACD,ACE,ADE,BCD,BCE,BDE,CDE"
--- src/recursion/TeamCombinations.java
+++ src/recursion/TeamCombinations.java
@@ -1,14 +1,47 @@
 package recursion;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class TeamCombinations {
 
-    public static List<String> listAllCombinations(List<String> args, int size)
+    public static void listAllCombinations(List<String> args, int size)
     {
-        // todo implement recursive method to get all combinations of a target size from a given set of chars
-        return Collections.emptyList();
+        doCombo(args, size, "");
+    }
+
+    public static void doCombo(List<String> args, int target, String ans)
+    {
+        /* take args, pull first item off string list
+        if string empty, consider it bad input & return back up call stack
+        tail-end recursive, pop the answer stack straight to output at the end
+
+        2023-08-07 getting index out of bounds... something to do w/ static method?
+        tried w/ char array & String impl, non-static. didn't seem to work.
+        2023-08-15 Figured out the solution was two-fold: first was moving empty args array check to AFTER the base
+        case; second was not copying the ans arg which was causing problems due to mutation. It seems when doing
+        recursions with Java I need to be mindful of the fact that Java is pass-by-reference-value.*/
+        if (target < 1) {
+            System.out.println(ans);
+        }
+        else if (args.isEmpty()) {
+            return;
+        }
+        else {
+            List<String> slice = args.subList(1, args.size());
+            String copy = ans;
+            doCombo(slice, target - 1, copy + args.get(0));
+            doCombo(slice, target, copy);
+        }
+    }
+
+    private static class Demo {
+
+        public static void main(String[] args)
+        {
+            listAllCombinations(Arrays.asList("a", "b", "c", "d", "e"), 3);
+        }
+
     }
 
 }
