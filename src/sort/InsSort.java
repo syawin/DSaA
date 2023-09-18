24aaf37a1f5581236da3da7f6620734900e5142e
Simon Yawin <simyawin@gmail.com>
2023-09-18 15:09:54 -0400

    up demo size

--- src/sort/InsSort.java
+++ src/sort/InsSort.java
@@ -13,6 +13,7 @@ public class InsSort extends SortableArray {
         for (int i = 1; i < arr.length; i++) {
             long temp = arr[i];
             int j = i;
+            // you can simplify this by moving the "shift" loop inside the while loop
             while (j > 0 && temp < arr[j - 1]) {
                 j--;
             }
@@ -27,7 +28,7 @@ public class InsSort extends SortableArray {
 
         public static void main(String[] args)
         {
-            int maxSize = 16;
+            int maxSize = 100;
             InsSort is = new InsSort(maxSize);
             for (int i = 0; i < maxSize; i++) {
                 long n = (int) (Math.random() * 99);
