c1d6fcbf4d2c2538906760e4df29ea1ae7d61655
Simon Yawin <simyawin@gmail.com>
2024-01-09 16:42:11 -0500

    Create INode class with Integer keys and Double values.

---
+++ src/tree/INode.java
@@ -0,0 +1,35 @@
+package tree;
+
+public class INode {
+
+    int    key;
+    double val;
+    INode  lChild;
+    INode  rChild;
+
+    public INode(int key, double val)
+    {
+        this.key = key;
+        this.val = val;
+    }
+
+    public INode(int key, double val, INode lChild, INode rChild)
+    {
+        this.key    = key;
+        this.val    = val;
+        this.lChild = lChild;
+        this.rChild = rChild;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Node{" +
+                       "key=" + key +
+                       ", val=" + val +
+                       ", lChild=" + lChild +
+                       ", rChild=" + rChild +
+                       '}';
+    }
+
+}
