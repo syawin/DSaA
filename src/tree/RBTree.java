36e2f9be5d564ee67b0c1ec9f664af6eeb5ad58e
Simon Yawin <simyawin@gmail.com>
2024-02-15 12:48:10 -0500

    Implement insert method for root case, implement flips for root case

--- src/tree/RBTree.java
+++ src/tree/RBTree.java
@@ -1,36 +1,62 @@
 package tree;
 
 public class RBTree extends Tree {
-
+    
     public RBTree()
     {
     }
-
+    
     public RBTree(TreeNode root)
     {
         super(root);
     }
-
+    
     @Override
     public void insert(int key, Object val)
     {
+        RBTreeNode insert = new RBTreeNode.Builder(key, val).build();
         if (this.getRoot() == null) {
-            this.setRoot(new RBTreeNode.Builder(key, val).red(false)
-                                                         .build());
+            insert.setRed(false);
+            this.setRoot(insert);
         }
-        // todo insert binary search find correct node algo
+        else {
+            RBTreeNode curr = ( RBTreeNode ) this.getRoot();
+            RBTreeNode paren;
+            if (curr.isBlack() & curr.hasTwoRedChildren()) {
+                curr.flipColors();
+                curr.setRed(false);
+            }
+            while (true) {
+                paren = curr;
+                if (key < curr.getKey()) {
+                    curr = ( RBTreeNode ) curr.getlChild();
+                    if (curr == null) {
+                        paren.setlChild(insert);
+                        return;
+                    }
+                }
+                else {
+                    curr = ( RBTreeNode ) curr.getrChild();
+                    if (curr == null) {
+                        paren.setrChild(insert);
+                        return;
+                    }
+                }
+            }
+        }
         // todo color flips on the way down
         // todo rotations on the way down
         // todo rotations after the node is inserted
     }
-
+    
     private static class RBTreeDemo {
-
+        
         public static void main(String[] args)
         {
-
+            RBTree rbTree = new RBTree();
+            rbTree.insert(1, 'A');
         }
-
+        
     }
-
+    
 }
--- test/tree/RBTreeTest.java
+++ test/tree/RBTreeTest.java
@@ -2,8 +2,37 @@ package tree;
 
 import org.junit.Test;
 
-public class RBTreeTest {
+import static org.junit.Assert.assertTrue;
 
+public class RBTreeTest {
+    
+    @Test
+    public void insertWhenRootIsBlackWithTwoRedChildren()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        RBTreeNode root;
+        root = (( RBTreeNode ) rbTree.getRoot());
+        assertTrue("root node is not black", root.isBlack());
+        
+        rbTree.insert(25, 'B');
+        rbTree.insert(75, 'C');
+        root = ( RBTreeNode ) rbTree.getRoot();
+        assertTrue("root node is not black", root.isBlack());
+        assertTrue("Left Child is not red", (( RBTreeNode ) root.getlChild()).isRed());
+        assertTrue("Right Child is not red", (( RBTreeNode ) root.getrChild()).isRed());
+        
+        rbTree.insert(37, 'D');
+        root = ( RBTreeNode ) rbTree.getRoot();
+        assertTrue("Left child is not black after color flip", (( RBTreeNode ) root.getlChild()).isBlack());
+        assertTrue("Right child is not black after color flip", (( RBTreeNode ) root.getrChild()).isBlack());
+        assertTrue(
+                "Newly inserted node is not red",
+                (( RBTreeNode ) root.getlChild()
+                                    .getrChild()).isRed()
+                  );
+    }
+    
     @Test
     public void insertWhenRootIsNull()
     {
@@ -12,7 +41,7 @@ public class RBTreeTest {
         rbTree.insert(1, 'A');
         assert rbTree.getRoot()
                      .getKey() == 1;
-        assert ((RBTreeNode) rbTree.getRoot()).isBlack();
+        assertTrue("root node is not black", (( RBTreeNode ) rbTree.getRoot()).isBlack());
     }
-
+    
 }
