448d728df8187688c5e74a3e3790b06625d6a61c
Simon Yawin <simyawin@gmail.com>
2024-03-20 16:39:19 -0400

    Impl RBTree insert & testing methods

--- src/tree/RBTree.java
+++ src/tree/RBTree.java
@@ -1,10 +1,14 @@
 package tree;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+
+import static common.CommonConstants.LINE_BREAK;
 
 public class RBTree extends Tree {
     
-    private static final Predicate<RBTreeNode> isFlip = n -> n.isBlack() && n.hasTwoRedChildren();
+    private static final Predicate<RBTreeNode>               isFlip        = n -> n.isBlack() && n.hasTwoRedChildren();
+    private static final BiPredicate<RBTreeNode, RBTreeNode> isRRViolation = (x, p) -> x.isRed() && p.isRed();
     
     public RBTree()
     {
@@ -15,52 +19,218 @@ public class RBTree extends Tree {
         super(root);
     }
     
+    // todo implement delete method
+    @Override
+    public boolean delete(int key)
+    {
+        return false;
+    }
+    
     @Override
     public void insert(int key, Object val)
     {
         RBTreeNode insert = new RBTreeNode.Builder(key, val).build();
+        Boolean isLeftChild = null, isLeftGrandChild = null;
+        System.out.println("Ins:\t" + insert);
         if (this.getRoot() == null) {
             insert.setRed(false);
             this.setRoot(insert);
-        }
-        else {
+        } else {
             RBTreeNode curr = ( RBTreeNode ) this.getRoot();
-            RBTreeNode paren;
+            RBTreeNode paren = null, gp = null;
             if (isFlip.test(curr)) {
                 curr.flipColors();
                 curr.setRed(false);
             }
             while (true) {
+                gp = paren;
                 paren = curr;
-                if (isFlip.test(curr)) curr.flipColors();
+                // begin seek subroutine
                 if (key < curr.getKey()) {
                     curr = ( RBTreeNode ) curr.getlChild();
+                    // paren is P, paren.L is X1 & paren.R is X2. this will only happen when P has 2 children
+                    if (isFlip.test(paren)) paren.flipColors();
+                    if (gp != null && isRRViolation.test(paren, gp)) {
+                        logRedRedViolation(paren, gp);
+                        rotateOnWayDown(paren, gp, isLeftGrandChild, isLeftChild);
+                    }
+                    isLeftGrandChild = isLeftChild;
+                    isLeftChild      = true;
                     if (curr == null) {
                         paren.setlChild(insert);
-                        return;
+                        insert.setParent(paren);
+                        curr = ( RBTreeNode ) paren.getlChild();
+                        break;
                     }
-                }
-                else {
+                } else {
                     curr = ( RBTreeNode ) curr.getrChild();
+                    if (isFlip.test(paren)) paren.flipColors();
+                    if (gp != null && isRRViolation.test(paren, gp)) {
+                        logRedRedViolation(paren, gp);
+                        rotateOnWayDown(paren, gp, isLeftGrandChild, isLeftChild);
+                    }
+                    isLeftGrandChild = isLeftChild;
+                    isLeftChild      = false;
                     if (curr == null) {
                         paren.setrChild(insert);
-                        return;
+                        insert.setParent(paren);
+                        curr = ( RBTreeNode ) paren.getrChild();
+                        break;
                     }
                 }
+                // end seek
+            }
+            
+            // after node insert, curr can be considered  X, p is Parent, g is GP
+            System.out.println("X:\t " + curr);
+            System.out.println("P:\t " + paren);
+            System.out.println("G:\t " + gp);
+            // at this point g, p, and x should all be set to the vals we need to properly do the final rotation
+            // test for 3 cases:
+            //     if P is black, nothing to do
+            if (gp != null && isRRViolation.test(curr, paren)) {
+                logRedRedViolation(curr, paren);
+                rotateAfterInsert(paren, isLeftGrandChild, isLeftChild, gp, insert);
             }
         }
-        // todo rotations on the way down
-        // todo rotations after the node is inserted
+        this.displayTree();
+        System.out.println(LINE_BREAK);
     }
     
-    // todo public boolean validateRBCorrect()
+    private void logRedRedViolation(RBTreeNode curr, RBTreeNode paren)
+    {
+        this.displayTree();
+        System.out.println("R-R violation between " + curr.getVal() + " and " + paren.getVal());
+    }
+    
+    private void rotateOnWayDown(RBTreeNode paren, RBTreeNode gp, Boolean isLeftGrandChild, Boolean isLeftChild)
+    {
+        if (isLeftGrandChild != null) {
+            // begin case outer gc
+            RBTreeNode ggp = ( RBTreeNode ) gp.getParent();
+            TreeNode top, temp = ggp.getParent();
+            if ((isLeftChild && isLeftGrandChild) || (!isLeftChild && !isLeftGrandChild)) {
+                gp.toggleNodeColor();
+                ggp.toggleNodeColor();
+                if (isLeftChild) {
+                    // do right rotation to raise P
+                    top = ggp.rotateRight();
+                } else {
+                    top = ggp.rotateLeft();
+                }
+                // GGP is top of rotation
+                // after rotation the node returned by the method must either be reattached to
+                // its parent or the root node. can determine if root if ggp.paren == null
+                // if ggp paren is null, update root
+            }
+            // end case outer gc
+            // begin case inner gc
+            else {
+                paren.toggleNodeColor();
+                ggp.toggleNodeColor();
+                // rotate GP in the direction that raises P
+                // then rotate ggp in the direction that raises P
+                if (isLeftChild) {
+                    // then isLeftGC must be false
+                    // rotate GP right
+                    // rotate GGP left
+                    ggp.setrChild(gp.rotateRight());
+                    ggp.getrChild()
+                       .setParent(ggp);
+                    top = ggp.rotateLeft();
+                } else {
+                    // then isLeftGC must be true
+                    // rotate GP left
+                    // rotate GGP right
+                    ggp.setlChild(gp.rotateLeft());
+                    ggp.getlChild()
+                       .setParent(ggp);
+                    top = ggp.rotateRight();
+                }
+            }
+            // end case inner gc
+            top.setParent(temp);
+            if (temp == null) {
+                this.setRoot(top);
+            } else if (top.getKey() < temp.getKey()) {
+                temp.setlChild(top);
+            } else {
+                temp.setrChild(top);
+            }
+        }
+    }
+    
+    private void rotateAfterInsert(RBTreeNode paren,
+                                   Boolean isLeftGrandChild,
+                                   Boolean isLeftChild,
+                                   RBTreeNode gp,
+                                   RBTreeNode insert)
+    {
+        TreeNode top, temp = gp.getParent();
+        if (isLeftGrandChild != null) {
+            if ((isLeftChild && isLeftGrandChild) || (!isLeftChild && !isLeftGrandChild)) {
+                // case P is red and X is an outside grandchild
+                gp.toggleNodeColor();
+                paren.toggleNodeColor();
+                if (isLeftChild) {
+                    top = gp.rotateRight();
+                } else {
+                    top = gp.rotateLeft();
+                }
+            } else {
+                // case P is red and X is an inside grandchild
+                gp.toggleNodeColor();
+                insert.toggleNodeColor();
+                if (isLeftChild) {
+                    // if X is a left child then that means P is a right child
+                    // perform a right rotation on P then a left on G
+                    gp.setrChild(paren.rotateRight());
+                    top = gp.rotateLeft();
+                } else {
+                    // if X is a right child then that means P is a left child
+                    gp.setlChild(paren.rotateLeft());
+                    top = gp.rotateRight();
+                }
+            }
+            top.setParent(temp);
+            if (temp == null) {
+                this.setRoot(top);
+            } else if (top.getKey() < temp.getKey()) {
+                temp.setlChild(top);
+            } else {
+                temp.setrChild(top);
+            }
+        }
+    }
+    
+    // todo implement method to validate tree for red-black correctness
+    public boolean validateRBCorrect()
+    {
+        return (( RBTreeNode ) this.getRoot()).isBlack();
+    }
     
     private static class RBTreeDemo {
         
         public static void main(String[] args)
         {
             RBTree rbTree = new RBTree();
-            rbTree.insert(1, 'A');
+            rbTree.insert(50, 'A');
+            rbTree.insert(25, 'B');
+            rbTree.insert(75, 'C');
+            rbTree.insert(30, 'D');
+            rbTree.insert(19, 'D');
+            rbTree.displayTree();
+            rbTree.setRoot(rbTree.getRoot()
+                                 .rotateRight());
+            rbTree.displayTree();
+            rbTree.setRoot(rbTree.getRoot()
+                                 .rotateLeft());
+            rbTree.displayTree();
+            rbTree.setRoot(rbTree.getRoot()
+                                 .rotateLeft());
+            rbTree.displayTree();
+            rbTree.setRoot(rbTree.getRoot()
+                                 .rotateLeft());
         }
         
     }
--- test/tree/RBTreeTest.java
+++ test/tree/RBTreeTest.java
@@ -1,9 +1,13 @@
 package tree;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+// todo add assertions to each test case
+//      ideal is to impl validateRBCorrect & then use that for assertions
+//      basically validate RB correct before insert & after insert
 public class RBTreeTest {
     
     @Test
@@ -34,6 +38,192 @@ public class RBTreeTest {
     }
     
     @Test
+    public void insertRRViolationWithRedUncle()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(4, 'A');
+        rbTree.insert(2, 'B');
+        rbTree.insert(6, 'C');
+        rbTree.insert(1, 'D');
+        rbTree.insert(3, 'E');
+        rbTree.insert(5, 'F');
+        rbTree.insert(7, 'G');
+        rbTree.displayTree();
+        rbTree.insert(8, 'G');
+        rbTree.displayTree();
+    }
+    
+    @Test
+    public void insertRRViolationWithoutUncle()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(10, 'A');
+        rbTree.insert(5, 'B');
+        rbTree.insert(20, 'C');
+        rbTree.insert(15, 'D');
+        rbTree.insert(25, 'E');
+        rbTree.insert(30, 'F');
+        rbTree.displayTree();
+    }
+    
+    @Test
+    public void insertWithMultipleViolations()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        rbTree.insert(25, 'B');
+        rbTree.insert(75, 'C');
+        rbTree.insert(15, 'D');
+        rbTree.insert(30, 'E');
+        rbTree.insert(60, 'F');
+        rbTree.insert(85, 'G');
+        rbTree.insert(55, 'H');
+        rbTree.displayTree();
+        rbTree.insert(65, 'I');
+        rbTree.displayTree();
+    }
+    
+    @Test
+    public void insertWithoutViolations()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(1, 'A');
+        rbTree.insert(2, 'B');
+        rbTree.insert(3, 'C');
+    }
+    
+    @Test
+    public void testColorFlipsNotRoot()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        RBTreeNode root;
+        root = (( RBTreeNode ) rbTree.getRoot());
+        assertTrue("root node is not black", root.isBlack());
+        
+        rbTree.insert(25, 'B');
+        rbTree.insert(75, 'C');
+        root = ( RBTreeNode ) rbTree.getRoot();
+        assertTrue("root node is not black", root.isBlack());
+        assertTrue("Left Child is not red", (( RBTreeNode ) root.getlChild()).isRed());
+        assertTrue("Right Child is not red", (( RBTreeNode ) root.getrChild()).isRed());
+        
+        rbTree.insert(37, 'D');
+        root = ( RBTreeNode ) rbTree.getRoot();
+        assertTrue("Left child is not black after color flip", (( RBTreeNode ) root.getlChild()).isBlack());
+        assertTrue("Right child is not black after color flip", (( RBTreeNode ) root.getrChild()).isBlack());
+        assertTrue(
+                "Newly inserted node is not red",
+                (( RBTreeNode ) root.getlChild()
+                                    .getrChild()).isRed()
+                  );
+        rbTree.insert(22, 'E');
+        rbTree.displayTree();
+        rbTree.insert(21, 'F');
+        rbTree.displayTree();
+    }
+    
+    @Test
+    @Ignore
+    public void testRotateAfterInsert()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        RBTreeNode root;
+        root = (( RBTreeNode ) rbTree.getRoot());
+        assertTrue("root node is not black", root.isBlack());
+        
+        rbTree.insert(25, 'B');
+        rbTree.insert(75, 'C');
+        root = ( RBTreeNode ) rbTree.getRoot();
+        assertTrue("root node is not black", root.isBlack());
+        assertTrue("Left Child is not red", (( RBTreeNode ) root.getlChild()).isRed());
+        assertTrue("Right Child is not red", (( RBTreeNode ) root.getrChild()).isRed());
+        
+        rbTree.insert(37, 'D');
+        root = ( RBTreeNode ) rbTree.getRoot();
+        assertTrue("Left child is not black after color flip", (( RBTreeNode ) root.getlChild()).isBlack());
+        assertTrue("Right child is not black after color flip", (( RBTreeNode ) root.getrChild()).isBlack());
+        assertTrue(
+                "Newly inserted node is not red",
+                (( RBTreeNode ) root.getlChild()
+                                    .getrChild()).isRed()
+                  );
+        rbTree.insert(22, 'E');
+        rbTree.insert(21, 'F');
+        rbTree.insert(23, 'G');
+        rbTree.insert(20, 'H');
+    }
+    
+    @Test
+    public void testRotateAfterInsertSmall()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        RBTreeNode root;
+        root = (( RBTreeNode ) rbTree.getRoot());
+        assertTrue("root node is not black", root.isBlack());
+        
+        rbTree.insert(25, 'B');
+        rbTree.insert(75, 'C');
+        root = ( RBTreeNode ) rbTree.getRoot();
+        assertTrue("root node is not black", root.isBlack());
+        assertTrue("Left Child is not red", (( RBTreeNode ) root.getlChild()).isRed());
+        assertTrue("Right Child is not red", (( RBTreeNode ) root.getrChild()).isRed());
+        
+        rbTree.insert(22, 'D');
+        rbTree.displayTree();
+        rbTree.insert(20, 'E');
+        rbTree.displayTree();
+        
+        rbTree.insert(76, 'F');
+        rbTree.displayTree();
+        rbTree.insert(77, 'G');
+        rbTree.displayTree();
+        
+        rbTree.insert(70, 'D');
+        rbTree.displayTree();
+    }
+    
+    @Test
+    public void testRotateLeftInnerGrandChild()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        RBTreeNode root;
+        root = (( RBTreeNode ) rbTree.getRoot());
+        assertTrue("root node is not black", root.isBlack());
+        
+        rbTree.insert(25, 'B');
+        rbTree.insert(75, 'C');
+        root = ( RBTreeNode ) rbTree.getRoot();
+        assertTrue("root node is not black", root.isBlack());
+        assertTrue("Left Child is not red", (( RBTreeNode ) root.getlChild()).isRed());
+        assertTrue("Right Child is not red", (( RBTreeNode ) root.getrChild()).isRed());
+        
+        rbTree.insert(12, 'E');
+        rbTree.displayTree();
+        rbTree.insert(18, 'D');
+        rbTree.displayTree();
+    }
+    
+    @Test
+    public void testRotateOnWayDownLeftInnerGrandChild()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        rbTree.insert(25, 'C');
+        rbTree.insert(75, 'B');
+        rbTree.insert(85, 'E');
+        rbTree.insert(65, 'D');
+        rbTree.insert(60, 'F');
+        rbTree.insert(55, 'G');
+        rbTree.displayTree();
+        rbTree.insert(59, 'H');
+        rbTree.displayTree();
+    }
+    
+    @Test
     public void insertWhenRootIsNull()
     {
         RBTree rbTree = new RBTree();
@@ -44,4 +234,73 @@ public class RBTreeTest {
         assertTrue("root node is not black", (( RBTreeNode ) rbTree.getRoot()).isBlack());
     }
     
+    @Test
+    public void testRotateOnWayDownLeftOuterGrandChild()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        rbTree.insert(75, 'B');
+        rbTree.insert(25, 'C');
+        rbTree.insert(37, 'D');
+        rbTree.insert(12, 'E');
+        rbTree.insert(6, 'F');
+        rbTree.insert(18, 'G');
+        rbTree.displayTree();
+        rbTree.insert(3, 'H');
+        rbTree.displayTree();
+    }
+    
+    @Test
+    public void testRotateOnWayDownRightInnerGrandChild()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        rbTree.insert(25, 'C');
+        rbTree.insert(75, 'B');
+        rbTree.insert(12, 'E');
+        rbTree.insert(37, 'D');
+        rbTree.insert(31, 'F');
+        rbTree.insert(43, 'G');
+        rbTree.displayTree();
+        rbTree.insert(30, 'H');
+        rbTree.displayTree();
+    }
+    
+    @Test
+    public void testRotateOnWayDownRightOuterGrandChild()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        rbTree.insert(75, 'B');
+        rbTree.insert(25, 'C');
+        rbTree.insert(60, 'D');
+        rbTree.insert(90, 'E');
+        rbTree.insert(80, 'F');
+        rbTree.insert(95, 'G');
+        rbTree.displayTree();
+        rbTree.insert(99, 'H');
+        rbTree.displayTree();
+    }
+    
+    @Test
+    public void testRotateRightInnerGrandChild()
+    {
+        RBTree rbTree = new RBTree();
+        rbTree.insert(50, 'A');
+        RBTreeNode root;
+        root = (( RBTreeNode ) rbTree.getRoot());
+        assertTrue("root node is not black", root.isBlack());
+        
+        rbTree.insert(25, 'B');
+        rbTree.insert(75, 'C');
+        root = ( RBTreeNode ) rbTree.getRoot();
+        assertTrue("root node is not black", root.isBlack());
+        assertTrue("Left Child is not red", (( RBTreeNode ) root.getlChild()).isRed());
+        assertTrue("Right Child is not red", (( RBTreeNode ) root.getrChild()).isRed());
+        
+        rbTree.insert(80, 'E');
+        rbTree.displayTree();
+        rbTree.insert(76, 'D');
+        rbTree.displayTree();
+    }
 }
