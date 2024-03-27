c3df5b51213e5132656f55aeb29b83c3e6a204a9
Simon Yawin <simyawin@gmail.com>
2024-03-27 16:18:23 -0400

    Add generic data class with parameterized type

---
+++ src/tree/DataItem.java
@@ -0,0 +1,16 @@
+package tree;
+
+public final class DataItem<T> {
+    
+    private final T data;
+    
+    public DataItem(T data) { this.data = data; }
+    
+// getter
+    public T getData()
+    {
+        return data;
+    }
+// getter end
+
+}
