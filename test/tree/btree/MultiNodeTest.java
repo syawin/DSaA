f5dcc1492711c4ef9779adb24f36c5cadc18a493
Simon Yawin <simyawin@gmail.com>
2024-03-29 16:02:28 -0400

    Add spec for MultiNode#toString

---
+++ test/tree/btree/MultiNodeTest.java
@@ -0,0 +1,19 @@
+package tree.btree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MultiNodeTest {
+    
+    @Test
+    public void testToString()
+    {
+        MultiNode<Long> multiNode = new MultiNode<>();
+        assertEquals("toString on empty Multinode should be '[]'", "[]", multiNode.toString());
+        DataItem<Long>[] itemArray = new DataItem[] {new NumericDataItem<>(1L), new NumericDataItem<>(2L)};
+        multiNode.setItemArray(itemArray);
+        assertEquals("toString on non-empty Multinode should be '[/1//2/]'", "[/1//2/]", multiNode.toString());
+    }
+    
+}
