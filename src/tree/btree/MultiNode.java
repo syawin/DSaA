6be5e26d312b70004614aea0e402302241c63505
Simon Yawin <simyawin@gmail.com>
2024-03-29 15:36:14 -0400

    Add toString to visualize BTrees

--- src/tree/btree/MultiNode.java
+++ src/tree/btree/MultiNode.java
@@ -1,8 +1,43 @@
 package tree.btree;
 
+import java.util.Arrays;
+
 public class MultiNode<T> {
     
     private DataItem<T>[]  itemArray;
     private MultiNode<T>[] childArray;
+    
+// getter
+    public MultiNode<T>[] getChildArray()
+    {
+        return childArray;
+    }
+    
+    public DataItem<T>[] getItemArray()
+    {
+        return itemArray;
+    }
+// getter end
+
+// setter
+    public void setChildArray(MultiNode<T>[] childArray)
+    {
+        this.childArray = childArray;
+    }
+    
+    public void setItemArray(DataItem<T>[] itemArray)
+    {
+        this.itemArray = itemArray;
+    }
+// setter end
+    
+    @Override
+    public String toString()
+    {
+        return (itemArray == null
+                ? "[]"
+                : Arrays.asList(itemArray)
+                        .toString());
+    }
     
 }
