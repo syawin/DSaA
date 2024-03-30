00030316153502b8e856d16c59f1044f75870d0a
Simon Yawin <simyawin@gmail.com>
2024-03-30 16:59:01 -0400

    Remove ignored spec

--- test/tree/RBTreeTest.java
+++ test/tree/RBTreeTest.java
@@ -1,6 +1,5 @@
 package tree;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -138,39 +137,6 @@ public class RBTreeTest {
         assertTrue("Tree is not RB correct", rbTree.validateRBCorrect());
         rbTree.insert(21, 'F');
         rbTree.displayTree();
-        assertTrue("Tree is not RB correct", rbTree.validateRBCorrect());
-    }
-    
-    @Test
-    @Ignore
-    public void testRotateAfterInsert()
-    {
-        RBTree rbTree = new RBTree();
-        rbTree.insert(50, 'A');
-        RBTreeNode root;
-        root = (( RBTreeNode ) rbTree.getRoot());
-        assertTrue("root node is not black", root.isBlack());
-        
-        rbTree.insert(25, 'B');
-        rbTree.insert(75, 'C');
-        root = ( RBTreeNode ) rbTree.getRoot();
-        assertTrue("root node is not black", root.isBlack());
-        assertTrue("Left Child is not red", (( RBTreeNode ) root.getlChild()).isRed());
-        assertTrue("Right Child is not red", (( RBTreeNode ) root.getrChild()).isRed());
-        
-        rbTree.insert(37, 'D');
-        root = ( RBTreeNode ) rbTree.getRoot();
-        assertTrue("Left child is not black after color flip", (( RBTreeNode ) root.getlChild()).isBlack());
-        assertTrue("Right child is not black after color flip", (( RBTreeNode ) root.getrChild()).isBlack());
-        assertTrue(
-                "Newly inserted node is not red",
-                (( RBTreeNode ) root.getlChild()
-                                    .getrChild()).isRed()
-                  );
-        rbTree.insert(22, 'E');
-        rbTree.insert(21, 'F');
-        rbTree.insert(23, 'G');
-        rbTree.insert(20, 'H');
         assertTrue("Tree is not RB correct", rbTree.validateRBCorrect());
     }
     
