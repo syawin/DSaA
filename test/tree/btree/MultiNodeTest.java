c2527f00c29a8949e97d0cdaa319918893b3c15e
Simon Yawin <simyawin@gmail.com>
2024-03-30 17:59:13 -0400

    Add exception specs

--- test/tree/btree/MultiNodeTest.java
+++ test/tree/btree/MultiNodeTest.java
@@ -1,5 +1,6 @@
 package tree.btree;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -8,12 +9,32 @@ import static org.junit.Assert.assertEquals;
 
 public class MultiNodeTest {
     
+    private static final int             ORDER = 4;
+    private              MultiNode<Long> multiNode;
+    
+    @Before
+    public void setUp()
+    {
+        multiNode = new MultiNode<>(ORDER);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void connectChildIndexLessThanZero()
+    {
+        multiNode.connectChild(-1, null);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testConnectChildGreaterThanOrEqualToOrder()
+    {
+        multiNode.connectChild(ORDER, null);
+    }
+    
     @Test
     public void testToString()
     {
-        MultiNode<Number> multiNode = new MultiNode<>(4);
         assertEquals("toString on empty Multinode should be '[]'", "[]", multiNode.toString());
-        List<DataItem<Number>> itemArray = List.of(new NumericDataItem<>(1L), new NumericDataItem<>(2L));
+        List<DataItem<Long>> itemArray = List.of(new NumericDataItem<>(1L), new NumericDataItem<>(2L));
         multiNode.setItemArray(itemArray);
         assertEquals("toString on non-empty Multinode should be '[/1//2/]'", "[/1//2/]", multiNode.toString());
     }
