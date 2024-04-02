9b23fbc848104f4723431e63b8dce7d13f336023
Simon Yawin <simyawin@gmail.com>
2024-04-02 16:03:35 -0400

    Add data item

---
+++ src/tree/btree/DataItem.java
@@ -0,0 +1,15 @@
+package tree.btree;
+
+public class DataItem {
+    
+    private final long key;
+    
+    public DataItem(long key) { this.key = key; }
+    
+    // getter
+    public long getKey()
+    {
+        return key;
+    }
+    // getter end
+}
