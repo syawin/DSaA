73f3a02d157641b476c37194b577f9a76b5ef69d
Simon Yawin <simyawin@gmail.com>
2024-04-25 16:37:23 -0400

    Add string formatting code & constants

--- src/common/CommonConstants.java
+++ src/common/CommonConstants.java
@@ -2,6 +2,13 @@ package common;
 
 public interface CommonConstants {
     
-    String LINE_BREAK = "•".repeat(100);
+    String         LINE_BREAK = "•".repeat(100);
+    StringFunction comma      = (s) -> s.replace(",", "")
+                                        .replaceAll("\s+", "");
+    
+    static String formatStr(String str, StringFunction func)
+    {
+        return func.run(str);
+    }
     
 }
