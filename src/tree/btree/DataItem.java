ce86b555dd0598d9ffb096892c77342bcad85f86
Simon Yawin <simyawin@gmail.com>
2024-05-14 16:18:54 -0400

    Impl equals for DataItem

--- src/tree/btree/DataItem.java
+++ src/tree/btree/DataItem.java
@@ -5,6 +5,15 @@ import org.jetbrains.annotations.NotNull;
 public record DataItem(long key) implements Comparable<DataItem> {
     
     @Override
+    public boolean equals(Object obj)
+    {
+        if (obj instanceof DataItem other) {
+            return this.compareTo(other) == 0;
+        }
+        return false;
+    }
+    
+    @Override
     public int compareTo(@NotNull DataItem o)
     {
         return Long.compare(this.key, o.key);
