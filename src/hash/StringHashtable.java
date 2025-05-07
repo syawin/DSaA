527be76279790b1838a4699811c7b16b56fe6476
Simon Yawin <simyawin@gmail.com>
2025-05-07 16:35:00 -0400

    #4 - Implement remaining hashtable methods.

--- src/hash/StringHashtable.java
+++ src/hash/StringHashtable.java
@@ -3,7 +3,8 @@ package hash;
 public class StringHashtable extends GenericHashtable<String, String> {
     
     private final int      ARRAY_SIZE;
-    private final int      PRIME_CONSTANT = 7;
+    private final int      PRIME_CONSTANT = 7; // todo this should be dynamically chosen from the
+    //  size
     private final String[] hashArray;
     
     public StringHashtable(int size)
@@ -15,6 +16,12 @@ public class StringHashtable extends GenericHashtable<String, String> {
     @Override
     public String delete(String key)
     {
+        int index = getHash(key);
+        while (hashArray[index] != null) {
+            if (hashArray[index].equals(key)) return hashArray[index];
+            index++;
+            index %= ARRAY_SIZE;
+        }
         return "";
     }
     
@@ -32,19 +39,34 @@ public class StringHashtable extends GenericHashtable<String, String> {
     @Override
     public String find(String key)
     {
+        int index = getHash(key);
+        while (hashArray[index] != null) {
+            if (hashArray[index].equals(key)) return hashArray[index];
+            index++;
+            index %= ARRAY_SIZE;
+        }
         return "";
     }
     
     @Override
     public void insert(String key)
     {
+        int hash = getHash(key);
+        while (hashArray[hash] != null) {
+            hash++;
+            hash %= ARRAY_SIZE;
+        }
+        hashArray[hash] = key;
+    }
+    
+    private int getHash(String key)
+    {
         int hash = 0;
         // iterate through the string as a char array
         for (int i = 0; i < key.length(); i++) {
             hash = (hash * PRIME_CONSTANT + key.charAt(i)) % ARRAY_SIZE;
         }
-        while (hashArray[hash] != null) { hash++; }
-        hashArray[hash] = key;
+        return hash;
     }
     
 }
