6c93de233c6c50a0b840e985ffa5c0ca8524cea2
Simon Yawin <simyawin@gmail.com>
2025-05-29 15:24:41 -0400

    Ensure unique random numbers in HeapSort and display the sorted array

--- src/heap/HeapSort.java
+++ src/heap/HeapSort.java
@@ -5,12 +5,49 @@ import tree.Node;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
 public class HeapSort {
     
     private static BufferedReader reader = null;
     
-    // getter
+    public static void main(String[] args)
+    {
+        int size, i;
+        System.out.print("Enter number of items: ");
+        size = getInt();
+        Heap heap = new Heap(size);
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
+        
+        for (i = 0; i < size; i++) {
+            int random;
+            do {
+                random = ( int ) (Math.random() * 100);
+            }
+            while (!uniqueNumbers.add(random));
+            heap.insertAt(i, new Node.Builder(random, null).build());
+            heap.incrementSize();
+        }
+        
+        System.out.print("Random: ");
+        heap.displayArray();
+        
+        for (i = (size / 2) - 1; i >= 0; i--) {
+            heap.trickleDown(i);
+        }
+        
+        System.out.print("Heap: ");
+        heap.displayArray();
+        heap.displayHeap();
+        
+        for (i = size - 1; i >= 0; i--) {
+            Node bigNode = heap.remove();
+            heap.insertAt(i, bigNode);
+        }
+        System.out.println("Sorted: ");
+        heap.displayArray();
+    }
+    
     private static int getInt()
     {
         return Integer.parseInt(getLine());
@@ -31,37 +68,6 @@ public class HeapSort {
             reader = new BufferedReader(new InputStreamReader(System.in));
         }
         return reader;
-    }
-    // getter end
-    
-    public static void main(String[] args)
-    {
-        int size, i;
-        System.out.print("Enter number of items: ");
-        size = getInt();
-        Heap heap = new Heap(size);
-        
-        for (i = 0; i < size; i++) {
-            int random = ( int ) (Math.random() * 100);
-            heap.insertAt(i, new Node.Builder(random, null).build());
-            heap.incrementSize();
-        }
-        
-        System.out.print("Random: ");
-        heap.displayArray();
-        
-        for (i = (size / 2) - 1; i >= 0; i--) {
-            heap.trickleDown(i);
-        }
-        
-        System.out.print("Heap: ");
-        heap.displayArray();
-        heap.displayHeap();
-        
-        for (i = size - 1; i >= 0; i--) {
-            Node bigNode = heap.remove();
-            heap.insertAt(i, bigNode);
-        }
     }
     
 }
