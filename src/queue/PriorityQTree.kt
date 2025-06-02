76d71cc616629a68633574b984386ec337ffedb5
Simon Yawin <simyawin@gmail.com>
2025-06-02 17:02:09 -0400

    #17 Implement tree heap - Convert priorityq to kotlin

--- src/queue/PriorityQTree.kt
+++ src/queue/PriorityQTree.kt
@@ -1,42 +1,21 @@
-package queue;
+package queue
 
-import tree.INode;
-import tree.ITree;
+import tree.INode
+import tree.ITree
 
-public class PriorityQTree {
-    
-    private ITree tree;
-    
-    public PriorityQTree()
-    {
-        tree = new ITree();
-    }
-    
-    // getter
-    public ITree getTree()
-    {
-        return tree;
-    }
-    // getter end
-    
-    // setter
-    public void setTree(ITree tree)
-    {
-        this.tree = tree;
-    }
+class PriorityQTree {
+    var tree: ITree = ITree()
+
     // setter end
-    
-    public void insert(int key) {
-        tree.insert(key,0.0);
+    fun insert(key: Int) {
+        tree.insert(key, 0.0)
     }
-    
-    public INode peek() {
-        return tree.max();
-    }
- 
-    public INode removeMox() {
-        INode max = tree.max();
-        tree.delete(max.getKey());
-        return max;
+
+    fun peek(): INode = tree.max()
+
+    fun removeMox(): INode {
+        val max = tree.max()
+        tree.delete(max.key)
+        return max
     }
 }
