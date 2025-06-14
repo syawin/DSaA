79d86c0e7768548451ffd1f917734b3f05137496
Simon Yawin <simyawin@gmail.com>
2025-06-14 16:36:51 -0400

    #17 Implement tree heap - Add isLeaf function to SimpleNode

--- src/tree/SimpleNode.kt
+++ src/tree/SimpleNode.kt
@@ -3,6 +3,8 @@ package tree
 data class SimpleNode(
     var key: Int? = null,
 ) {
+    fun isLeaf(): Boolean = left == null && right == null
+
     var left: SimpleNode? = null
     var right: SimpleNode? = null
     var parent: SimpleNode? = null
