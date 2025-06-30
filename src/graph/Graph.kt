c417e787ba16bf2669e8dd2317ca52368ee7699b
Simon Yawin <simyawin@gmail.com>
2025-06-30 18:00:01 -0400

    #24 Min Span Tree w BFS - Add minimumSpanningTreeBFS method stub and expand graph example for testing MST with BFS.

--- src/graph/Graph.kt
+++ src/graph/Graph.kt
@@ -38,9 +38,7 @@ open class Graph(
         collection.add(0)
         while (!collection.isEmpty) {
             val currentVertex = collection.peek()
-
             val adjacentVertex = getAdjacentUnvisitedVertexIndex(currentVertex)
-
             if (adjacentVertex == -1) {
                 collection.remove()
             } else {
@@ -57,7 +55,6 @@ open class Graph(
         val collection = IntegerStack(maxSize)
         vertexList[0]?.wasVisited = true
         collection.add(0)
-
         while (!collection.isEmpty) {
             val currentVertex = collection.peek()
             val v = getAdjacentUnvisitedVertexIndex(currentVertex)
@@ -66,9 +63,27 @@ open class Graph(
             } else {
                 vertexList[v]?.wasVisited = true
                 collection.add(v)
-
                 displayVertex(currentVertex)
                 displayVertex(v)
+                print(" ")
+            }
+        }
+        println()
+        resetFlags()
+    }
+
+    fun minimumSpanningTreeBFS() {
+        val collection = IntegerQ(maxSize)
+        vertexList[0]?.wasVisited = true
+        collection.add(0)
+        var v2: Int
+        while (!collection.isEmpty) {
+            val currentVertex = collection.remove()
+            while ((getAdjacentUnvisitedVertexIndex(currentVertex).also { v2 = it }) != -1) {
+                vertexList[v2]?.wasVisited = true
+                collection.add(v2)
+                displayVertex(currentVertex)
+                displayVertex(v2)
                 print(" ")
             }
         }
@@ -106,23 +121,6 @@ open class Graph(
             if (adjMatrix[vertIndex][i] && (vertexList[i]?.wasVisited) == false) return i
         }
         return -1
-    }
-
-    fun minimumSpanningTreeBFS() {
-        val collection = IntegerQ(maxSize)
-        vertexList[0]?.wasVisited = true
-        collection.add(0)
-        var v2: Int
-        while (!collection.isEmpty) {
-            val currentVertex = collection.remove()
-            while ((getAdjacentUnvisitedVertexIndex(currentVertex).also { v2 = it }) != -1) {
-                collection.add(v2)
-                vertexList[v2]?.wasVisited = true
-                displayVertex(v2)
-            }
-        }
-        println()
-        resetFlags()
     }
 }
 
