2b7b0018154fb7cff1477a482dd84349e86d0ad6
Simon Yawin <simyawin@gmail.com>
2025-07-27 16:20:05 -0400

    #35 Implement Minimum Spanning Tree Program - Enhance `Edge` class to implement `Comparable`; override `equals`, `hashCode`, and `toString` for equality, hashing, and string representation.

--- src/graph/Edge.kt
+++ src/graph/Edge.kt
@@ -4,4 +4,42 @@ data class Edge(
     val source: Int,
     val destination: Int,
     val weight: Int = 1,
-)
+) : Comparable<Edge> {
+    override fun compareTo(other: Edge): Int = this.weight - other.weight
+
+    /**
+     * Indicates whether some other object is "equal to" this one. Implementations must fulfil the following
+     * requirements:
+     *
+     * * Reflexive: for any non-null value `x`, `x.equals(x)` should return true.
+     * * Symmetric: for any non-null values `x` and `y`, `x.equals(y)` should return true if and only if `y.equals(x)` returns true.
+     * * Transitive:  for any non-null values `x`, `y`, and `z`, if `x.equals(y)` returns true and `y.equals(z)` returns true, then `x.equals(z)` should return true.
+     * * Consistent:  for any non-null values `x` and `y`, multiple invocations of `x.equals(y)` consistently return true or consistently return false, provided no information used in `equals` comparisons on the objects is modified.
+     * * Never equal to null: for any non-null value `x`, `x.equals(null)` should return false.
+     *
+     * Read more about [equality](https://kotlinlang.org/docs/reference/equality.html) in Kotlin.
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Edge) return false
+        return source == other.source && destination == other.destination && weight == other.weight
+    }
+
+    /**
+     * Returns a hash code value for the object.  The general contract of `hashCode` is:
+     *
+     * * Whenever it is invoked on the same object more than once, the `hashCode` method must consistently return the same integer, provided no information used in `equals` comparisons on the object is modified.
+     * * If two objects are equal according to the `equals()` method, then calling the `hashCode` method on each of the two objects must produce the same integer result.
+     */
+    override fun hashCode(): Int {
+        var result = source
+        result = 31 * result + destination
+        result = 31 * result + weight
+        return result
+    }
+
+    /**
+     * Returns a string representation of the object.
+     */
+    override fun toString(): String = "($source, $destination, $weight)"
+}
